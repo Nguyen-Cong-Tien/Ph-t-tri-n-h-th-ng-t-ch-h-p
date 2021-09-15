b4753bcb3f9de80365e4b17ea9ef5a02ce5ce46f
Nguyen-Cong-Tien <90307540+Nguyen-Cong-Tien@users.noreply.github.com>
2021-09-15 14:41:13 +0700

    Bài 1 java cơ bản

---
+++ Bai1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a;
+        System.out.println("Nhap vao chuoi: ");
+        a= sc.nextLine();
+        System.out.println("Chuoi duoc in hoa thanh:");
+        System.out.println(a.toUpperCase());
+    }
+}
